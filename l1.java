5b819da5176eb12391fe04d280e605d92bf90b98
hainl2000 <66208534+hainl2000@users.noreply.github.com>
2021-02-24 23:26:18 +0700

    Add files via upload

---
+++ l1.java
@@ -0,0 +1,20 @@
+package bk;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class l1 {
+    public static void main(String[] args) throws IOException {
+//        FirstDegree1Var eq = new FirstDegree1Var();
+//        eq.setA(0);
+//        eq.setB(10);
+//        eq.show();
+//        System.out.println(eq.resolve());
+
+//        SecondDegree eq2 = new SecondDegree();
+//        eq2.setA(0);
+//        eq2.setB(0);
+//        eq2.setC(0);
+//        System.out.println(eq2.resolve());
+    }
+}
